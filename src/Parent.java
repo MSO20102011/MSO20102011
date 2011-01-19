8c75df063938aca722295aca603ef1a79afb6aef
Bart Spaans <bart.spaans@gmail.com>
2011-01-19 14:40:57 +0100

    Added Rutger's work, except for his Data class, which was already supplied by the MSO website (see the utils folder)

---
+++ src/Parent.java
@@ -0,0 +1,39 @@
+
+public class Parent {
+	
+	int allowedMisses;
+	int missesCount;
+	float ownedMoney;
+	
+	public void addChild(Child child) {
+		
+	}
+	public void removeChild(Child child) {
+		
+	}
+	public void viewLunchSchedule(Child child) {
+		
+	}
+	public void addLunchDate(Child child, Date date) {
+		
+	}
+	public void remLunchDate(Child child, Date date) {
+		
+	}
+	public void viewAvailability() {
+		
+	}
+	public void addAvailability(Date date, Boolean preferred) {
+		
+	}
+	public void changeAvailability(Date date, Boolean preferred) {
+		
+	}
+	public void removeAvailability(Date date) {
+		
+	}
+	public void swapTurn(SwapRequest swap) {
+		
+	}
+
+}
---
+++ src/ScheduledTasks.java
@@ -0,0 +1,17 @@
+
+public class ScheduledTasks {
+
+	public void calculateFine() {
+		
+	}
+	public void calculateCost() {
+		
+	}
+	public void calculateSupervisionSchedule(int month, int year) {
+		
+	}
+	public void resetMissedMeetings() {
+		
+	}
+	
+}
